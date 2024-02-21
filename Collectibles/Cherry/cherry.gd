1ef5ea6e158f1f108ce0ce6222a313ddf97f518b
Fernando Freitas <freitax.li@gmail.com>
2024-02-21 18:54:38 -0300

    Criação de collectibles

---
+++ Collectibles/Cherry/cherry.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+func _on_body_entered(body):
+	if body.name == "Jogador":
+		# adiciona 3 de vida, limitando em 10 a vida final
+		Game.playerHP = min(10, (Game.playerHP + 3))
+
+		var tweenPosicao = get_tree().create_tween()
+		var tweenOpacidade = get_tree().create_tween()
+		
+		# serve para cereja flutuar no ar
+		tweenPosicao.tween_property(self, "position", position - Vector2(0, 25), .3)
+		
+		# diminui a opacidade do node do padrão até 0
+		tweenOpacidade.tween_property(self, "modulate:a", 0, .3)
+		
+		# função para quando terminar de executar as propriedades
+		tweenPosicao.tween_callback(queue_free)
--- main.gd
+++ main.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+func _ready():
+	Util.saveGame()
+	Util.loadGame()
+
 func _on_botao_sair_pressed():
 	get_tree().quit()
 
 func _on_botao_jogar_pressed():
 	Util.loadGame() 
+	
+	# reseta a vida e ouro para quando ele morrer de vez
+	if Game.playerHP <= 0:
+		Game.playerHP = 10
+		Game.playerGold = 0
+		
 	get_tree().change_scene_to_file("res://mundo.tscn")
